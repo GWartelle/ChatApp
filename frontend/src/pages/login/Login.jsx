0feb306a556c7f42ba6a5e25b5034c4a03a22b57
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-03-09 18:39:17 +0100

    Signup page implementation

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -6,6 +6,7 @@ const Login = () => {
           Login
           <span className="text-blue-500"> ChatApp</span>
         </h1>
+
         <form>
           <div>
             <label className="label p-2">
@@ -13,29 +14,30 @@ const Login = () => {
             </label>
             <input
               type="text"
-              placeholder="Enter username"
+              placeholder="Enter Username"
               className="w-full input input-bordered h-10"
             />
           </div>
 
           <div>
-            <label className="label">
+            <label className="label p-2">
               <span className="text-base label-text">Password</span>
             </label>
             <input
-              type="text"
-              placeholder="Enter password"
+              type="password"
+              placeholder="Enter Password"
               className="w-full input input-bordered h-10"
             />
           </div>
+
           <a
             href="#"
-            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            className="text-sm hover:underline hover:text-blue-600 mt-4 p-2 inline-block"
           >
             {"Don't"} have an account?
           </a>
           <div>
-            <button className="btn btn-block btn-sm mt-2">Login</button>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700">Login</button>
           </div>
         </form>
       </div>
