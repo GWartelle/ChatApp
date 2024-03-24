48a9371cfd48e433d6dca615f3d8817a59c15e03
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-03-24 01:04:36 +0100

    Removing Unused code

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -39,25 +39,3 @@ const MessageInput = () => {
 };
 
 export default MessageInput;
-
-// STARTER CODE SNIPPET:
-// import { BsSend } from "react-icons/bs";
-
-// const MessageInput = () => {
-//   return (
-//     <form className="px-4 my-3">
-//       <div className="w-full">
-//         <input
-//           type="text"
-//           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
-//           placeholder="Send a message"
-//         />
-//         <button>
-//           <BsSend />
-//         </button>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default MessageInput;
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -19,16 +19,3 @@ const LogoutButton = () => {
 };
 
 export default LogoutButton;
-
-// STARTER CODE SNIPPET :
-// import { BiLogOut } from "react-icons/bi";
-
-// const LogoutButton = () => {
-//   return (
-//     <div className="mt-auto">
-//       <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
-//     </div>
-//   );
-// };
-
-// export default LogoutButton;
