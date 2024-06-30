7d9964df426e54ee74e737c2948ffd05c48c515f
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-06-30 19:35:09 +0200

    LogoutButton Update

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -9,6 +9,7 @@ const LogoutButton = () => {
       {!loading ? (
         <BiLogOut
           className="w-6 h-6 text-white cursor-pointer"
+          title="Log Out"
           onClick={logout}
         />
       ) : (
