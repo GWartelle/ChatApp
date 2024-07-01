b8dd76e88852c894497778fa3d47efdd4aaeade6
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-01 12:47:37 +0200

    DeleteButton update

--- frontend/src/components/sidebar/DeleteButton.jsx
+++ frontend/src/components/sidebar/DeleteButton.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { BiTrash } from "react-icons/bi";
 import useDelete from "../../hooks/useDelete";
+import ConfirmationModal from "../modals/ConfirmationModal";
 
 const DeleteButton = () => {
   const { loading, deleteAccount } = useDelete();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDeleteAccount = () => {
+    deleteAccount();
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
@@ -10,11 +18,16 @@ const DeleteButton = () => {
         <BiTrash
           className="w-6 h-6 text-white cursor-pointer"
           title="Delete Account"
-          onClick={deleteAccount}
+          onClick={() => setIsModalOpen(true)}
         />
       ) : (
         <span className="loading loading-spinner"></span>
       )}
+      <ConfirmationModal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        onConfirm={handleDeleteAccount}
+      />
     </div>
   );
 };
