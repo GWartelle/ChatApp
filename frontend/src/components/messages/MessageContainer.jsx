9d73f9b7a98fca98c2d3d9222b3a7be46b984dbb
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-01 16:54:02 +0200

    Welcome message update

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,6 +4,7 @@ import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
@@ -41,7 +42,9 @@ const NoChatSelected = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 {authUser.fullName} ❄️</p>
+        <p>
+          Welcome 👋 {authUser.fullName} {getRandomEmoji()}
+        </p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
