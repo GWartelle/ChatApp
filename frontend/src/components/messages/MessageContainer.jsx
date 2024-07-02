677754575de2e05503692d8d268c73176f018dbf
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-02 12:22:18 +0200

    MessageContainer max width setting

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -15,7 +15,7 @@ const MessageContainer = () => {
   }, [setSelectedConversation]);
 
   return (
-    <div className="md:min-w-[450px] flex flex-col">
+    <div className="md:min-w-[450px] max-w-80 flex flex-col">
       {!selectedConversation ? (
         <NoChatSelected />
       ) : (
