17514c7ed5891428ae9c11e76da4e5de85b20c49
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-02 13:39:44 +0200

    Revert "useSidebar zustand state setup"
    This reverts commit bfb02760ba7fb18a64e9e1449688508bfe945032.

--- frontend/src/zustand/useSidebar.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useSidebar = create((set) => ({
-  isSidebarOpen: true,
-  setIsSidebarOpen: (bool) => set(() => ({ isSidebarOpen: bool })),
-}));
-
-export default useSidebar;
