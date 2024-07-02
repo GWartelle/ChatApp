4309c83ce9d3eacd7b81abec979e80759306dc38
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-02 13:34:53 +0200

    Revert "Sidebar update"
    This reverts commit 704fd0c489072e61baccdc1c150206b4ae15dbf0.

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -2,15 +2,10 @@ import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 import DeleteButton from "./DeleteButton";
-import useSidebar from "../../zustand/useSidebar";
 
 const Sidebar = () => {
-  const { isSidebarOpen } = useSidebar();
-
   return (
-    <div
-      className={`border-r border-slate-500 p-4 flex flex-col transition-all duration-300 ease-in-out ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
-    >
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
